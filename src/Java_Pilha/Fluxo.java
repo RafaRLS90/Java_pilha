3977ffce797d40ba479dbbbef8552583a968287b
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-03-02 22:07:55 -0300

    mudando o try e catch de posição

--- src/Java_Pilha/Fluxo.java
+++ src/Java_Pilha/Fluxo.java
@@ -9,7 +9,12 @@ public class Fluxo {
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-        metodo2();
+       try {
+    	   metodo2();
+       } catch(ArithmeticException ex) {
+    	   System.out.println("ArithmeticException");
+       }
+       
         System.out.println("Fim do metodo1");
     }
 
@@ -17,13 +22,9 @@ public class Fluxo {
         System.out.println("Ini do metodo2");
         for(int i = 1; i <= 5; i++) {
             System.out.println(i);
-            try {
+         
             	int a = i / 0;
-            } catch(ArithmeticException ex) {
-            	System.out.println("ArithmeticException");
             }
-            
-        }
         System.out.println("Fim do metodo2");
     }
 }
