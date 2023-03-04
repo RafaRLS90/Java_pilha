06be33b9eb62119ffe350b2ebfe406570be69176
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-03-04 15:24:23 -0300

    lançando exceções

--- src/Java_Pilha/Fluxo.java
+++ src/Java_Pilha/Fluxo.java
@@ -3,35 +3,28 @@ package Java_Pilha;
 public class Fluxo {
 	public static void main(String[] args) {
         System.out.println("Ini do main");
-        metodo1();
+        try {
+     	   metodo1();
+        } catch(ArithmeticException |NullPointerException  ex) {
+     	   String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+     	   ex.printStackTrace();
+        
+        } 
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
         System.out.println("Ini do metodo1");
-       try {
-    	   metodo2();
-       } catch(ArithmeticException |NullPointerException  ex) {
-    	   String msg = ex.getMessage();
-           System.out.println("ArithmeticException " + msg);
-    	   System.out.println("ArithmeticException");
-    	   ex.printStackTrace();
-       
-       } 
-       
+        metodo2();
         System.out.println("Fim do metodo1");
     }
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println(i);
-         
-            	int a = i / 0;
-            	//Conta c = null;
-            	//c.deposita();
-            
-            }
-        System.out.println("Fim do metodo2");
+        
+        throw new ArithmeticException("deu errado");
+       
+        //System.out.println("Fim do metodo2");
     }
 }
