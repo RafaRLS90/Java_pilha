7aca09326a59fdfd328e89447b86e820c6781bbc
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-03-05 11:45:43 -0300

    heranças e suas características

--- src/Java_Pilha/Conta.java
+++ src/Java_Pilha/Conta.java
@@ -1,5 +1,7 @@
 package Java_Pilha;
 
 public class Conta {
-	void deposita() {}
+	void deposita() throws MinhaExcecao{
+		
+	}
 }
---
+++ src/Java_Pilha/TextaComExcecaoChecked.java
@@ -0,0 +1,17 @@
+package Java_Pilha;
+
+public class TextaComExcecaoChecked {
+
+	public static void main(String[] args) {
+		
+		Conta c = new Conta();
+		try {
+			c.deposita();
+		} catch(MinhaExcecao ex) {
+			System.out.println("tratamento....");
+		}
+
+
+	}
+
+}
